9479e84e51bfef3eb37a2af9b9aab5f9753b144d
adamnnagy <an2208@nyu.edu>
2017-04-12 17:37:56 -0400

    Update sketch.js
    additional changes after: 
    April 12. 5:37

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ var a = 200;
 function setup(){
 
   createCanvas(600*0.71, 600);
+	rectMode(CENTER);
 
   // noStroke();
 
@@ -21,12 +22,24 @@ function draw() {
   // // rotate(2*PI/3);
   // rect(mouseX, mouseY, height, height);
 
-
+// if the rectangle is centered, the translate should happen in the middle of the screen
+	
   translate(width/2, height/2);
-  rect(mouseX, mouseY, a, a);
-
+	
+	//not according to the mouse anymore
+	
+	//could analyze sound in the final project -- and do the coloring according to that. 
+	//also playing the instrument: nature
+	
+	//rotate rectangle 1
+	rotate(radians(frameCount));
+//   rect(mouseX, mouseY, a, a);
+	rect(0, 0, a, a);
   // scale(2/3);
-  rotate(PI/3);
+  
+	//here I can implement a for cycle, which takes a rectangle or a series of them in an array.
+	//the counter will offset the rotation, the fill and the scaling. -->  map()?
+	
 
   rect(mouseX, mouseY, a, a);
 }
