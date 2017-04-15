aabd5ec05ff6db7415fb9cff25269a26f87e1983
adamnnagy <an2208@nyu.edu>
2017-04-15 18:00:59 -0400

    A working version

--- sketch.js
+++ sketch.js
@@ -1,16 +1,26 @@
+
 var rectangles = [];
-var defaultHeight = 50;
-var numberOfRectangles = 5;
+var defaultHeight = 75;
+var numberOfRectangles = 30;
 
 function setup(){
-
-  createCanvas(600*0.71, 600);
+  colorMode(RGB);
+  // createCanvas(600*0.71, 600);
+  createCanvas(windowWidth, windowHeight);
 	rectMode(CENTER);
+  ellipseMode(CENTER);
 
   // noStroke();
 
+  colorFrom1 = color(36, 27, 45);
+  colorTo1 = color(25, 82, 99);
+
+  colorFrom2 = color(18, 103, 122);
+  colorTo2 = color(10, 54, 65);
+
+
   for (var i = 0; i < numberOfRectangles; i++) {
-    rectangles[i] = new Rectangle(defaultHeight*numberOfRectangles*i);
+    rectangles[i] = new Rectangle(defaultHeight*i);
     console.log(rectangles[i].width);
   }
 
@@ -22,19 +32,32 @@ function draw() {
 
   translate(width/2, height/2);
 
-  rect(0, 0, defaultHeight, defaultHeight);
-  rect(0, 0, rectangles[1].height, rectangles[1].width);
+
 
 
 for (var i = rectangles.length - 1; i > 0; i--) {
-  // noStroke();
 
-  //map the color!
-  
-  fill(27, 76, 90*(i%255));
-  // scale(i);
+  strokeWeight(0.05);
+  stroke(14, 92, 111);
+  gradient = map(i, rectangles.length - 1, 1, 1, 0);
+  stroke(lerpColor(colorFrom2, colorTo2, gradient));
+
+  fill(lerpColor(colorFrom1, colorTo1, gradient));
   rectangles[i].update();
   rectangles[i].display();
+
+
+
+}
+
+for (var i = 0; i < 8; i++) {
+
+  rotate(TWO_PI*i/8);
+  strokeWeight(0.1);
+  stroke(20);
+  fill(247, 245, 237, 130);
+  ellipse(0, 17, 5);
+
 }
 
 	//could analyze sound in the final project -- and do the coloring according to that.
